e2557075d90936d4a39e267e4ca28c1c538f1f69
Balaji <balajikaredla4@gmail.com>
2026-03-04 17:15:40 +0530

    some lexing

---
+++ src/frontend/lexer.rs
@@ -0,0 +1,9 @@
+use santiago::lexer::LexerRules;
+
+pub fn lexer_rules() -> LexerRules {
+    santiago::lexer_rules!(
+        "DEFAULT" | "INT" = pattern r"[0-9]+";
+        "DEFAULT" | "PLUS" = string "+";
+        "DEFAULT" | "WS" = pattern r"\s" => |lexer| lexer.skip();
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use rs_clp::frontend::lexer::lexer_rules;
+
 fn main() {
+    let input = "10 + 20 + 30";
+
+    let lexer_rules = lexer_rules();
+    let lexemes = santiago::lexer::lex(&lexer_rules, &input).unwrap();
+
+    for lexeme in lexemes {
+        println!("{}", *lexeme);
+    }
+
     println!("Hello, world!");
 }
